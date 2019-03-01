7bf99bdaea4b772aefce9ea23be21bc9c675b770
caiks <caiks@greenlake.co.uk>
2019-03-01 18:57:16 +0000

    model4

---
+++ NIST_engine4.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE DeriveGeneric, OverloadedStrings, BangPatterns, ScopedTypeVariables #-}
+
+
+import Control.Monad
+import Control.Monad.ST
+import Data.STRef
+import Data.List hiding (union)
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Array.Repa as R
+import qualified Data.Array.Repa.Shape as RS
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BL8
+import qualified Codec.Compression.GZip as GZip
+import Data.Word
+import Data.Aeson hiding (Value)
+import System.Random
+import System.IO
+import Data.Time
+import System.Locale
+import Text.Printf
+import GHC.Real
+import Data.Csv
+import Control.Applicative
+import GHC.Generics
+
+import AlignmentUtil
+import Alignment
+import AlignmentApprox
+import AlignmentRandom
+import AlignmentPracticable
+import AlignmentPracticableIO
+import AlignmentAeson
+import AlignmentAesonPretty
+import AlignmentRepaVShape
+import AlignmentRepa
+import AlignmentAesonRepa
+import AlignmentRandomRepa
+import AlignmentPracticableRepa
+import AlignmentPracticableIORepa
+import AlignmentDevRepa hiding (aahr)
+import NISTDev
+
+main :: IO ()
+main = 
+  do
+    printf ">>>\n"
+    hFlush stdout
+
+    (uu,hr) <- nistTrainBucketedIO 2
+
+    printf "train size: %d\n" $ hrsize hr
+    hFlush stdout
+
+    let digit = VarStr "digit"
+    let vv = uvars uu
+    let vvl = sgl digit
+    let vvk = vv `minus` vvl
+
+    let model = "NIST_model4"
+    let (wmax,lmax,xmax,omax,bmax,mmax,umax,pmax,fmax,mult,seed) = (2^11, 8, 2^10, 60, (60*3), 3, 2^8, 1, 127, 1, 5)
+
+    printf ">>> %s\n" $ model
+    Just (uu1,df) <- decomperIO uu vvk hr wmax lmax xmax omax bmax mmax umax pmax fmax mult seed
+    BL.writeFile (model ++ ".json") $ decompFudsPersistentsEncode $ decompFudsPersistent df
+    printf "<<< done %s\n" $ model
+    hFlush stdout
+
+    printf "decomp size: %d\n" $ (Set.size $ fvars $ dfff df)
+
+    let (a,ad) = summation mult seed uu1 df hr
+    printf "alignment: %.2f\n" $ a
+    printf "alignment density: %.2f\n" $ ad
+
+    printf "<<< done\n"
+
+
