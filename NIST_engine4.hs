e6d62cdcde60ac9c1ebd67a00cebc94b91945e41
caiks <caiks@greenlake.co.uk>
2019-05-27 16:00:38 +0100

    new shuffle

--- NIST_engine4.hs
+++ NIST_engine4.hs
@@ -63,16 +63,32 @@ main =
     let (wmax,lmax,xmax,omax,bmax,mmax,umax,pmax,fmax,mult,seed) = (2^11, 8, 2^10, 60, (60*3), 3, 2^8, 1, 127, 1, 5)
 
     printf ">>> %s\n" $ model
-    Just (uu1,df) <- decomperIO uu vvk hr wmax lmax xmax omax bmax mmax umax pmax fmax mult seed
-    BL.writeFile (model ++ ".json") $ decompFudsPersistentsEncode $ decompFudsPersistent df
+    Just (uu1,df1) <- decomperIO uu vvk hr wmax lmax xmax omax bmax mmax umax pmax fmax mult seed
+    BL.writeFile (model ++ ".json") $ decompFudsPersistentsEncode $ decompFudsPersistent df1
     printf "<<< done %s\n" $ model
     hFlush stdout
 
-    printf "decomp size: %d\n" $ (Set.size $ fvars $ dfff df)
+    printf "model cardinality: %d\n" $ (Set.size $ fvars $ dfff df1)
 
-    let (a,ad) = summation mult seed uu1 df hr
+    let hr' = hrev [i | i <- [0.. hrsize hr - 1], i `mod` 8 == 0] hr
+
+    printf "selected train size: %d\n" $ hrsize hr'
+    hFlush stdout
+
+    let (a,ad) = summation mult seed uu1 df1 hr'
     printf "alignment: %.2f\n" $ a
     printf "alignment density: %.2f\n" $ ad
+    hFlush stdout
+
+    let pp = qqll $ treesPaths $ hrmult uu1 df1 hr'
+
+    bmwrite (model ++ ".bmp") $ bmvstack $ map (\bm -> bminsert (bmempty (28+2) ((28+2)*(maximum (map length pp)))) 0 0 bm) $ map (bmhstack . map (\(_,hrs) -> bmborder 1 (hrbm 28 1 2 (hrs `hrhrred` vvk)))) $ pp
+    printf "bitmap %s\n" $ model ++ ".bmp"
+    hFlush stdout
+
+    bmwrite (model ++ "_2.bmp") $ bmvstack $ map (\bm -> bminsert (bmempty ((28*2)+2) (((28*2)+2)*(maximum (map length pp)))) 0 0 bm) $ map (bmhstack . map (\((_,ff),hrs) -> bmborder 1 (bmmax (hrbm 28 2 2 (hrs `hrhrred` vvk)) 0 0 (hrbm 28 2 2 (qqhr 2 uu vvk (fund ff)))))) $ pp
+    printf "bitmap %s\n" $ model ++ "_2.bmp"
+    hFlush stdout
 
     printf "<<< done\n"
 
